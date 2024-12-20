38ba1daac4679c1936f7d03f0b2e25665f2ff592
Dylan <Dylan@example.com>
2024-12-19 20:10:55 -0400

    Examen_FInal

---
+++ Examen_Final_Compiladores_Escobar_SIles_Dylan/1_d_Examen_Final/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    public class Estado
+    {
+        public const int q0 = 0;
+        public const int q1 = 1;
+        public const int q2 = 2;
+        public const int q3 = 3;
+    }
+
+    public void Valido(string s)
+    {
+        if (Correcto(s))
+        {
+            Console.WriteLine("Es válido");
+        }
+        else
+        {
+            Console.WriteLine("No es válido");
+        }
+    }
+
+    public bool Correcto(string s)
+    {
+        int actual = Estado.q0;
+        foreach (char i in s)
+        {
+            switch (actual)
+            {
+                case Estado.q0:
+                    if (i == 'm') actual = Estado.q1;
+                    else if (i == 'h') actual = Estado.q2;
+                    else return false;
+                    break;
+
+                case Estado.q1:
+                    if (i == 'h') actual = Estado.q3;
+                    else if (i == 'a') actual = Estado.q2;
+                    else return false;
+                    break;
+
+                case Estado.q2:
+                    if (i == 'm') actual = Estado.q1;
+                    break;
+
+                case Estado.q3:
+                    if (i == 'a') actual = Estado.q3;
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+        return actual == Estado.q2 || actual == Estado.q3;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese una cadena:");
+        string s = Console.ReadLine();
+
+            Program program = new Program();
+            program.Valido(s);
+
+    }
+}
---
+++ Examen_Final_Compiladores_Escobar_SIles_Dylan/3_d_Examen_Final/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public string s;
+    public int index = 0;
+    List<string> directorios = new List<string> { "home", "var", "etc"};
+    List<string> extensiones = new List<string> { "txt", "docx", "exe", "rar", "zip" };
+
+    public bool file()
+    {
+        if (path() && extension() && index == s.Length)
+        {
+            return true;
+        }
+        else if (path() && index == s.Length)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool path()
+    {
+        if (dir() && path_es())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool path_es()
+    {
+        if(s[index]=='.') return true;
+        if(s[index]!='/') return false;
+        index++; 
+        if (dir() && path_es())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool dir()
+    {
+        string p = "";
+        while (index<s.Length && s[index]!='.' && s[index] != '/')
+        {
+            p += s[index];
+            index++;
+        }   
+        if (directorios.Contains(p))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool extension()
+    {
+        if(s[index]!='.') return false;
+        index++;
+        string p="";
+        while (index < s.Length)
+        {
+            p += s[index];
+            index++;
+        }
+        if (extensiones.Contains(p))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void valido(string entrada)
+    {
+        s = entrada; 
+        index = 0;   
+        if (file())
+        {
+            Console.WriteLine("Es válido");
+        }
+        else
+        {
+            Console.WriteLine("No es válido");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese una cadena:");
+        string s = Console.ReadLine();
+        Program p = new Program();
+        p.valido(s);
+    }
+}
